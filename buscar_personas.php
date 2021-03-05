654a518ea071852e77cd8410589a79502752a28d
mari <mansumdp@gmail.com>
2021-03-04 21:14:58 -0300

    busqueda de personas hasta 50 resultados

--- buscar_personas.php
+++ buscar_personas.php
@@ -9,9 +9,9 @@ include './conexion/conexion.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon.png">
+    <link rel="shortcut icon" href="favicon.jpg">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Buscar Persona</title>
+    <title>Buscar Personas</title>
     <meta name="description" content="El sistema nº1 de analisis de datos de contribuyentes de América Latina">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
--- personas_buscadas.php
+++ personas_buscadas.php
@@ -1,13 +1,39 @@
 <?php
-include './conexion/conexion.php'
+include './conexion/conexion.php';
+
 
 $nombre = $_POST['var1'];
 $calle = $_POST['var2'];
 $localidad = $_POST['var3'];
+$sql = "select * from contactos where nombre like '%".$nombre."%' and dir_calle like '%".$calle."%' and localidad like '%".$localidad."%'";
 
-echo $nombre;
-echo $calle;
-echo $localidad;
+$mostrar = mysqli_query($link,$sql);
 
+$counter = mysqli_num_rows($mostrar);
+
+
+if($counter<50){
+echo "<table border=1>";
+echo "<tr>";
+echo "<td>NOMBRE</td>";
+echo "<td>CALLE</td>";
+echo "<td>NUMERO</td>";
+echo "<td>LOCALIDAD</td>";
+echo "</tr>";
+
+while($row = mysqli_fetch_assoc($mostrar)){
+echo "<tr>";
+echo "<td>".$row['nombre']."</td>";
+echo "<td>".$row['dir_calle']."</td>";
+echo "<td>".$row['dir_numero']."</td>";
+echo "<td>".$row['localidad']."</td>";
+echo "</tr>";
+
+}
+echo "</table>";
+}
+else{
+    echo "La busqueda arroja mas de 50 resultados";
+}
 
 ?>
