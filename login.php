bc7f6a12f2a36e1238c60a59bc4891d546336ed3
Marina <mansumdp@gmail.com>
2021-08-08 16:58:10 -0300

    cambios en commit

--- login.php
+++ login.php
@@ -14,12 +14,12 @@ session_start();
   <body class="text-center">
     <form class="form-signin" action = "access.php" method="post">
     <img src="icons8-lambda-100.png"/>
-          <h2 class="m-3 lambdaweb-title">Lambda Web</h2>
+      <h2 class="m-3 lambdaweb-title">Lambda Web</h2>
       <label for="username" class="sr-only">Nombre de usuario</label>
       <input type="text" id="username" class="form-control mb-3" placeholder="Nombre de usuario" required autofocus name="user" >
       <label for="inputPassword" class="sr-only">Contraseña</label>
       <input type="password" id="inputPassword" class="form-control mb-3" placeholder="Contraseña" required name="password">
-      <button class="btn btn-md btn-primary btn-block" type="submit">Ingresar</button>
+      <button class="btn btn-md  btn-primary btn-block" type="submit">Ingresar</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
     </form>
   </body>
